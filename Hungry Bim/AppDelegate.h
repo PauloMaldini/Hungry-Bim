9a1bed1ff550edd3d126827b50cf0641184adfb2
Kostya <user@Kostyas-MacBook.local>
2012-02-25 00:43:40 +0900

    Initial Commit

---
+++ Hungry Bim/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hungry Bim
+//
+//  Created by Konstantin Tsymbalist on 25.02.12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hungry Bim/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hungry Bim
+//
+//  Created by Konstantin Tsymbalist on 25.02.12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
